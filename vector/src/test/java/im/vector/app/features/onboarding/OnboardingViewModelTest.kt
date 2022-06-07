23d2934b74586068388f157fa2d2234eba879ce2
Adam Brown <adampsbrown@gmail.com>
2022-06-07 16:19:22 +0100

    taking into account the servers ability to sign out all devices

--- vector/src/test/java/im/vector/app/features/onboarding/OnboardingViewModelTest.kt
+++ vector/src/test/java/im/vector/app/features/onboarding/OnboardingViewModelTest.kt
@@ -464,7 +464,7 @@ class OnboardingViewModelTest {
                             copy(isLoading = false, resetState = resetState)
                         }
                 )
-                .assertEvents(OnboardingViewEvents.OnResetPasswordSendThreePidDone)
+                .assertEvents(OnboardingViewEvents.OnResetPasswordSendThreePidDone(AN_EMAIL))
                 .finish()
     }
 
